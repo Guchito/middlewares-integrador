4b522075c0ef64e284d5b70a5f69d27d2027b9a6
Agustin <a_gaggero@hotmail.com>
2020-11-24 21:01:07 -0300

    Es lo que hay chicas

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const bcryptjs = require('bcryptjs');
+const {validationResult} = require('express-validator');
+
+const file = path.join(__dirname, '../data/users.json');
+
+function getAllUsers() {
+	return JSON.parse(fs.readFileSync(file, 'utf-8'));
+}
+
+function generateNewId() {
+	const users = getAllUsers();
+	return users.pop().id + 1;
+}
+
+function writeUser(user) {
+	const users = getAllUsers();
+	const usersToSave = [...users, user];
+	const userToJson = JSON.stringify(usersToSave, null, " ");
+	fs.writeFileSync(file, userToJson);
+}
+
+
+
 module.exports = {
     showRegister: (req, res) => {
-        // Do the magic
-        return res.send('Do the magic');
+        return res.render('user/user-register-form');
     },
     processRegister: (req, res) => {
-        // Do the magic
+        let errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()){
+            return res.render('user/user-register-form', {errors: errors.errors})
+        }
+        const users = getAllUsers();
+
         return res.send('Do the magic');
     },
     showLogin: (req, res) => {
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const {check, body} = require('express-validator');
+const fs = require('fs');
 
 const userController = require('../controllers/userController');
 
 // Muestra la vista de registro
-router.get('/register', userController.showRegister);
+router.get('/register',  userController.showRegister);
 
 // Procesa la vista de registro
-router.post('/register', userController.processRegister);
+router.post('/register', [check('email').isEmail().withMessage('No pusiste un mail valido'),
+    body('email').custom(function(value){
+        const users = JSON.parse(fs.readFileSync('/../database/users.json', 'utf-8'));
+        const usersExist = users.find((user)=>{
+            return users.email == value
+        })
+        return usersExist ? true : false;
+
+        }).withMessage('Usuario ya existe'),
+    check('password').isLength({ min: 6 }).withMessage('Minmo 6 caracteres'),
+    
+    ] , userController.processRegister);
 
 // Muestra la vista de login
 router.get('/login', userController.showLogin);
